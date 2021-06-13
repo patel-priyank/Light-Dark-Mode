fd52c0b5964744f62fb46de1531b977ebecb2aac
Priyank Patel <patel23priyank@gmail.com>
2021-06-13 15:10:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `./img/undraw_proud_coder_${color}.svg`;
+  image2.src = `./img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function toggleDarkLightMode(theme) {
+  nav.style.backgroundColor = theme === DARK_THEME ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = theme === DARK_THEME ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+
+  toggleIcon.children[0].textContent = theme === DARK_THEME ? 'Dark Mode' : 'Light Mode';
+
+  theme === DARK_THEME
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+  theme === DARK_THEME ? imageMode('dark') : imageMode('light');
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    toggleDarkLightMode(DARK_THEME);
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    toggleDarkLightMode(LIGHT_THEME);
+  }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(DARK_THEME);
+  }
+}
